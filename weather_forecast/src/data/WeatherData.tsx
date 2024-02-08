4328a29a4c59433a50706442ae6c71a70c5e78ed
severo <oliveira.severo@gmail.com>
2024-02-07 22:18:49 -0300

    feat: :rocket: Cria o arquivo data que vai popular os cards

---
+++ weather_forecast/src/data/WeatherData.tsx
@@ -0,0 +1,130 @@
+const weatherData = [
+  {
+    id: 1,
+    weekday: 'Monday',
+    date: {
+      month: 'Jan',
+      day: 20,
+    },
+    temp: {
+      min: 19,
+      max: 25,
+    },
+    humidity: {
+      min: 19,
+      max: 25,
+    },
+    weather: '🌥️',
+    isSunny: false,
+  },
+  {
+    id: 2,
+    weekday: 'Tuesday',
+    date: {
+      month: 'Jan',
+      day: 21,
+    },
+    temp: {
+      min: 18,
+      max: 24,
+    },
+    humidity: {
+      min: 20,
+      max: 28,
+    },
+    weather: '☀️',
+    isSunny: true,
+  },
+  {
+    id: 3,
+    weekday: 'Wednesday',
+    date: {
+      month: 'Jan',
+      day: 22,
+    },
+    temp: {
+      min: 17,
+      max: 23,
+    },
+    humidity: {
+      min: 18,
+      max: 26,
+    },
+    weather: '🌧️',
+    isSunny: false,
+  },
+  {
+    id: 4,
+    weekday: 'Thursday',
+    date: {
+      month: 'Jan',
+      day: 23,
+    },
+    temp: {
+      min: 20,
+      max: 26,
+    },
+    humidity: {
+      min: 22,
+      max: 30,
+    },
+    weather: '⛈️',
+    isSunny: false,
+  },
+  {
+    id: 5,
+    weekday: 'Friday',
+    date: {
+      month: 'Jan',
+      day: 24,
+    },
+    temp: {
+      min: 22,
+      max: 28,
+    },
+    humidity: {
+      min: 24,
+      max: 32,
+    },
+    weather: '🌦️',
+    isSunny: false,
+  },
+  {
+    id: 6,
+    weekday: 'Saturday',
+    date: {
+      month: 'Jan',
+      day: 25,
+    },
+    temp: {
+      min: 21,
+      max: 27,
+    },
+    humidity: {
+      min: 20,
+      max: 28,
+    },
+    weather: '☀️',
+    isSunny: true,
+  },
+  {
+    id: 7,
+    weekday: 'Sunday',
+    date: {
+      month: 'Jan',
+      day: 25,
+    },
+    temp: {
+      min: 21,
+      max: 27,
+    },
+    humidity: {
+      min: 20,
+      max: 28,
+    },
+    weather: '☀️',
+    isSunny: true,
+  },
+];
+
+export default weatherData;
