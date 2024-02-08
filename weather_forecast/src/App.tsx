1ed9c89e2212cc28db0ac3cf0d253e3be9ad9d7e
severo <oliveira.severo@gmail.com>
2024-02-07 22:18:23 -0300

    feat: :lipstick: Cria a base do card list

--- weather_forecast/src/App.tsx
+++ weather_forecast/src/App.tsx
@@ -1,12 +1,13 @@
 import './CSS/reset.css';
 import './CSS/style.css';
+import CardList from './componentes/CardList.tsx';
 import Header from './componentes/Header.tsx';
 
 function App() {
   return (
     <main>
       <Header title="Previsão 360" />
-      <p>Início do projeto de previsão do tempo</p>
+      <CardList />
     </main>
   );
 }
---
+++ weather_forecast/src/componentes/CardList.tsx
@@ -0,0 +1,9 @@
+function CardList() {
+  return (
+    <div className="card-list">
+      <p>Um card aqui</p>
+    </div>
+  );
+}
+
+export default CardList;
