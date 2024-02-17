f36dd0ae486630b7378c7c1318b7c1c81c214cf0
severo <oliveira.severo@gmail.com>
2024-02-17 14:03:09 -0300

    feat: :rocket: Atualiza Readme e faz ajsutes no CSS

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # frontend_modelos
+
+![Prévia da página - Preview of the page](./tela_app.png)
+
+[Clique aqui](https://weather.severidade.com.br/) para verificar a versão final do projeto em seu navegador.
--- src/componentes/footer/index.tsx
+++ src/componentes/footer/index.tsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
   <footer>
     <p><strong className={ styles.titulo }> Tecnologias</strong> React - Javascript - Slick Carousel - HTML - CSS </p>
-    <p>Este projeto foi desenvolvido por Marco Severo</p>
+    <p>Este projeto foi desenvolvido por Marco Severo.</p>
     <ul className={ styles.menu_social }>
       <li><a className={ styles.social_linkedin } href="https://www.linkedin.com/in/severidade/">linkedin.com/in/severidade</a></li>
       <li><a className={ styles.social_github } href="https://github.com/severidade">github.com/severidade</a> </li>
