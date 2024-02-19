9254b0e23d05cc0b0038f8b7182c2f9dae830bb8
severo <oliveira.severo@gmail.com>
2024-02-19 13:07:40 -0300

    feat: :lipstick: Atualiza estilos dos slick dots

--- src/componentes/footer/index.tsx
+++ src/componentes/footer/index.tsx
@@ -6,10 +6,10 @@ function Footer() {
     <p><strong className={ styles.titulo }> Tecnologias</strong> React - Javascript - Slick Carousel - HTML - CSS </p>
     <p>Este projeto foi desenvolvido por Marco Severo.</p>
     <ul className={ styles.menu_social }>
-      <li><a className={ styles.social_linkedin } href="https://www.linkedin.com/in/severidade/">linkedin.com/in/severidade</a></li>
-      <li><a className={ styles.social_github } href="https://github.com/severidade">github.com/severidade</a> </li>
+      <li><a className={ styles.social_linkedin } target="_blank" href="https://www.linkedin.com/in/severidade/">linkedin.com/in/severidade</a></li>
+      <li><a className={ styles.social_github } target="_blank" href="https://github.com/severidade">github.com/severidade</a> </li>
       {/* <li><a class="social_codpen" href="https://codepen.io/severidade">codepen.io/severidade</a></li> */}
-      <li><a className={ styles.social_behance } href="https://www.behance.net/severidade">behance.net/severidade</a></li>
+      <li><a className={ styles.social_behance } target="_blank" href="https://www.behance.net/severidade">behance.net/severidade</a></li>
     </ul>
   </footer>
   );
