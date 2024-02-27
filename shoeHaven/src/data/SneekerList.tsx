8dcc76b60edbe23fe671ef263c3ee988f345376c
severo <oliveira.severo@gmail.com>
2024-02-27 00:17:54 -0300

    feat: :rocket: Cia o tipo e o objeto que irá alimentar os cards

---
+++ shoeHaven/src/data/SneekerList.tsx
@@ -0,0 +1,24 @@
+import { SneekerType } from '../types/SneekerType';
+
+const SneekerList: SneekerType[] = [
+  {
+    id: 1,
+    title: 'Nike',
+    image: '',
+    price: 450,
+  },
+  {
+    id: 2,
+    title: 'Louis Vuitton',
+    image: '',
+    price: 1000,
+  },
+  {
+    id: 3,
+    title: 'Nike legend',
+    image: '',
+    price: 450,
+  },
+];
+
+export default SneekerList;
