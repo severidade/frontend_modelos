4c9ea365741a6d2da6da0a037adbd3bb72623590
severo <oliveira.severo@gmail.com>
2024-02-27 10:10:52 -0300

    feat: :lipstick: Cria os estilos do carrosel
    CSS modules nao funciona no carrosel

--- shoeHaven/src/componentes/SneakerList/index.tsx
+++ shoeHaven/src/componentes/SneakerList/index.tsx
@@ -1,14 +1,28 @@
+import Slider from 'react-slick';
 import { SneakerType } from '../../types/SneakerType';
 import CardSneaker from '../CardSneaker';
 
-import styles from './SneakerList.module.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import './SneakerList.css';
 
 function SneakerList({ sneakers }: { sneakers: SneakerType[] }) {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+  };
+
   return (
-    <section className={ styles.container_card }>
-      { sneakers.map((sneaker) => (
-        <CardSneaker key={ sneaker.id } sneakerItem={ sneaker } />
-      ))}
+    <section className="container_card">
+      <Slider { ...settings }>
+        { sneakers.map((sneaker) => (
+          <CardSneaker key={ sneaker.id } sneakerItem={ sneaker } />
+        ))}
+      </Slider>
     </section>
   );
 }
