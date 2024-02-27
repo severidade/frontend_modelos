88c4241da72a2eb2aa58c381ba17e24407b8ad5d
severo <oliveira.severo@gmail.com>
2024-02-27 02:29:56 -0300

    feat: :rocket: Carrega todos os itens de um card

--- shoeHaven/src/componentes/SneakerList/index.tsx
+++ shoeHaven/src/componentes/SneakerList/index.tsx
@@ -2,18 +2,19 @@ import { SnekerType } from '../../types/SneakerType';
 
 function SneakerList({ sneakers }: { sneakers: SnekerType[] }) {
   return (
-    <section>
-      {/* <p> Aqui lista todos os cards </p> */}
-
-      <div>
+    <section className="container_card">
+      <div className="card">
         <h2 className="sneaker_price">
           {sneakers[5].title }
         </h2>
+        <p>{`${sneakers[5].manufacturer}`}</p>
         <figure>
           <img src={ sneakers[5].image } alt={ sneakers[5].title } />
         </figure>
         <p className="sneaker_price">
-          {sneakers[5].price }
+          {' '}
+          {sneakers[5].price}
+          {' '}
         </p>
       </div>
     </section>
