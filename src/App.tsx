8ab8da835d5750ce4e2ceba2511224236a8681e2
severo <oliveira.severo@gmail.com>
2024-07-04 16:26:07 -0300

    feat: :rocket: Consome dados em app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
 /* eslint-disable react/react-in-jsx-scope */
+import data from './data/index.ts';
 import './App.css';
 
 function App() {
   return (
     <>
-      ola
+      {data.map((category) => (
+        <div key={category.id}>
+          <h2>{category.name}</h2>
+          <ul>
+            {category.movies.map((movie) => (
+              <li key={movie.id}>
+                <p>
+                  {movie.title}
+                  {' '}
+                  (
+                  {movie.released}
+                  )
+                </p>
+                <iframe
+                  title={movie.title}
+                  width="560"
+                  height="315"
+                  src={`https://www.youtube.com/embed/${movie.embedId}`}
+                  frameBorder="0"
+                  allowFullScreen
+                />
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </>
   );
 }
