0071804cb6e996c28becfdc0b3b3818c824fa3cd
severo <oliveira.severo@gmail.com>
2024-07-04 16:25:11 -0300

    feat: :rocket: Cria arquivo que simila dados da api

---
+++ src/data/index.ts
@@ -0,0 +1,40 @@
+const categories = [
+  {
+    id: 1,
+    name: 'Action',
+    movies: [
+      {
+        id: 1, title: 'Mad Max: Fury Road', released: 2015, embedId: 'hEJnMQG9ev8',
+      },
+      {
+        id: 2, title: 'John Wick', released: 2014, embedId: 'gZtmzT9Wgrw',
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: 'Drama',
+    movies: [
+      {
+        id: 1, title: 'Forrest Gump', released: 1994, embedId: 'vDY_uZAaU7g',
+      },
+      {
+        id: 2, title: "Schindler's List", released: 1993, embedId: 'mxphAlJID9U',
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: 'Sci-fi',
+    movies: [
+      {
+        id: 1, title: 'Star Wars: Episode IV - A New Hope', released: 1977, embedId: 'MpkrMqmmy5k',
+      },
+      {
+        id: 3, title: 'Avatar: The Way of Water', released: 2023, embedId: 'd9MyW72ELq0 ',
+      },
+    ],
+  },
+];
+
+export default categories;
