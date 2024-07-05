30133c15163c50370f186a9c80cb14377485cb72
severo <oliveira.severo@gmail.com>
2024-07-05 14:18:15 -0300

    feat: :rocket: Cria um titulo para aplicacao

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,13 @@ import Sidebar from './components/Sidebar/index.tsx';
 
 function App() {
   // Inicializa o estado com o primeiro filme da primeira categoria
+
   const [selectedMovie, setSelectedMovie] = useState(data[0].movies[0]);
+  const [favoritList, setFavoritList] = useState([]);
 
   return (
     <div className="main">
+      <h1 className="app_title">CineHits</h1>
       <div className="contentWrapper">
         <Sidebar
           data={data}
