ebb280611d1bfa0b6ceb5f1a448f9fad1b45b99e
severo <oliveira.severo@gmail.com>
2024-07-05 01:05:24 -0300

    feat: :rocket: Tipa as props em sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,5 +1,14 @@
 /* eslint-disable react/react-in-jsx-scope */
-function Sidebar({ data, selectedMovie, setSelectedMovie }) {
+
+import { Movie } from '../../types/movie.ts';
+
+type SidebarProps = {
+  data: { id: number; name: string; movies: Movie[] }[]; // Tipo para o array de categorias
+  selectedMovie: Movie; // Tipo para o filme selecionado
+  setSelectedMovie: (movie: Movie) => void; // Tipo para a função que seleciona o filme
+};
+
+function Sidebar({ data, selectedMovie, setSelectedMovie }: SidebarProps) {
   return (
     <div className="sidebar">
       <h2>Lista de filmes por categoria</h2>
