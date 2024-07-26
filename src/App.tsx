427ad26158f2db18f0f190bd90737280ef837315
severo <oliveira.severo@gmail.com>
2024-07-26 00:02:26 -0300

    refactor: :recycle: Cria NavBar
    Componente agora contém o titulo da aplicaçao para alinhar com o menu hamburger

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import data from './data/index.ts';
 import './App.css';
 import Footer from './components/Footer/index.tsx';
 import Player from './components/Player/index.tsx';
-import Sidebar from './components/Sidebar/index.tsx';
+import NavBar from './components/NavBar/index.tsx';
 import { Movie } from './types/movie.ts';
 import OrientationDetectorDevice from './components/OrientationDetectorDevice/index.tsx';
 
@@ -31,17 +31,16 @@ function App() {
 
   return (
     <div className="main">
-      <h1 className="app_title">CineHits</h1>
+      <NavBar
+        data={data}
+        selectedMovie={selectedMovie}
+        setSelectedMovie={setSelectedMovie}
+        isPlaying={isPlaying}
+        togglePlayVideo={togglePlayVideo}
+        favoritList={favoritList}
+        toggleFavorite={toggleFavorite}
+      />
       <div className="contentWrapper">
-        <Sidebar
-          data={data}
-          selectedMovie={selectedMovie}
-          setSelectedMovie={setSelectedMovie}
-          isPlaying={isPlaying}
-          togglePlayVideo={togglePlayVideo}
-          favoritList={favoritList}
-          toggleFavorite={toggleFavorite}
-        />
         <Player
           selectedMovie={selectedMovie}
           favoritList={favoritList}
---
+++ src/components/NavBar/index.tsx
@@ -0,0 +1,88 @@
+/* eslint-disable react/react-in-jsx-scope */
+import { useState, useCallback, useEffect } from 'react';
+import { Movie } from '../../types/movie.ts';
+import { Data } from '../../types/data.ts';
+
+import './NavBar.css';
+import SidebarButton from '../SidebarButton/index.tsx';
+import { useDeviceInfo } from '../../utils/useDeviceInfo.tsx';
+import MenuHamburger from '../MenuHamburger/index.tsx';
+
+type NavBarProps = {
+  data: Data[]; // Tipo para o array de categorias
+  selectedMovie: Movie; // Tipo para o filme selecionadowindowWidth
+  setSelectedMovie: (movie: Movie) => void; // Tipo para a função que seleciona o filme
+  favoritList: string[];
+  isPlaying: boolean;
+  toggleFavorite: (movieTitle: string) => void;
+  togglePlayVideo: () => void;
+};
+
+function NavBar({
+  data, selectedMovie, setSelectedMovie, favoritList, isPlaying, toggleFavorite, togglePlayVideo,
+}: NavBarProps) {
+  const { isMobile } = useDeviceInfo();
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = useCallback(() => {
+    setMenuOpen((prevMenuOpen) => !prevMenuOpen);
+  }, []);
+
+  const handleMenuClick = useCallback(() => {
+    if (isPlaying) {
+      togglePlayVideo();
+    }
+    toggleMenu();
+  }, [isPlaying, toggleMenu, togglePlayVideo]);
+
+  useEffect(() => {
+    if (!isMobile) {
+      setMenuOpen(false);
+    }
+  }, [isMobile]);
+
+  return (
+
+    // <nav className="container_sidebar"></nav>
+    <nav>
+      <div className="nav-header">
+        <h1 className="app_title">CineHits</h1>
+        <MenuHamburger
+          isMobile={isMobile}
+          menuOpen={menuOpen}
+          handleMenuClick={handleMenuClick}
+        />
+      </div>
+      <div className="container_sidebar">
+        <div className={`menu ${menuOpen ? 'open' : ''}`}>
+          {data.map((category) => (
+            <div key={category.id}>
+              <h3 className="film_category">{category.name}</h3>
+              <div className="film_list">
+                {category.movies.map((movie) => (
+                  <SidebarButton
+                    key={movie.id}
+                    movie={movie}
+                    isSelected={selectedMovie.title === movie.title}
+                    isFavorite={favoritList.includes(movie.title)}
+                    onClick={() => {
+                      setSelectedMovie(movie);
+                      if (isMobile) {
+                        toggleMenu();
+                      } else if (isPlaying) {
+                        togglePlayVideo();
+                      }
+                    }}
+                    toggleFavorite={toggleFavorite}
+                  />
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
