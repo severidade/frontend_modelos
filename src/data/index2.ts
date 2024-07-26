52748608ad5f9c3e6223d400aa3a9d3a18c27262
severo <oliveira.severo@gmail.com>
2024-07-26 00:49:55 -0300

    feat: :rocket: Teste de uma nova lista de filmes

---
+++ src/data/index2.ts
@@ -0,0 +1,83 @@
+const categories = [
+  {
+    id: 1,
+    name: 'Animation',
+    movies: [
+      {
+        id: 1, title: 'Luca', released: 2021, embedId: 'yNG9Hw0IY4I',
+      },
+      {
+        id: 2, title: 'Spirited Away', released: 2001, embedId: 'ByXuk9QqQkk',
+      },
+      {
+        id: 3, title: 'My Neighbor Totoro', released: 1988, embedId: '92a7Hj0ijLs',
+      },
+      {
+        id: 4, title: 'The Iron Giant', released: 1999, embedId: 'obLtyj8hfFk',
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: 'Comedy',
+    movies: [
+      {
+        id: 5, title: 'The Flintstones', released: 1950, embedId: 'iStl4Al48BA',
+      },
+      {
+        id: 6, title: 'Shrek', released: 1950, embedId: 'cbMgEtAIXPU',
+      },
+      {
+        id: 7, title: 'The Simpsons', released: 1960, embedId: '5iGG6Cpui7o',
+      },
+      {
+        id: 8, title: 'Sponge Bob', released: 1950, embedId: 'py6aEUjT8uY',
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: 'Aventura',
+    movies: [
+      {
+        id: 7, title: 'Alice in Wonderland', released: 1950, embedId: 'yGCPhLXJ4hI',
+      },
+      {
+        id: 7, title: 'Beetlejuice', released: 1950, embedId: 'kU1PAvdGeI8',
+      },
+      {
+        id: 8, title: 'The Incredibles', released: 1954, embedId: 'I7pVNcjclx8',
+      },
+      {
+        id: 8, title: 'The Powerpuff Girls', released: 1960, embedId: '7qmPEg8rgjA',
+      },
+      {
+        id: 8, title: 'Toy Story', released: 1961, embedId: 'nDe7uVm9TAk',
+      },
+    ],
+  },
+  {
+    id: 4,
+    name: 'Sci-fi',
+    movies: [
+      {
+        id: 2, title: 'The Fifth Element', released: 1950, embedId: 'VGxfLsCrW7I',
+      },
+      {
+        id: 3, title: 'Return of the Jedi', released: 1950, embedId: 'm7Xcpofvm7M',
+      },
+      {
+        id: 4, title: 'He Man - The Masters of the Universe', released: 1980, embedId: 'D-FVPr7lNQM',
+      },
+      {
+        id: 5, title: 'Star Wars: The Phantom Menace ', released: 1980, embedId: '0uqhAoMkFUc',
+      },
+      {
+        id: 7, title: 'The Matrix ', released: 1980, embedId: 'i5jCFWlGlnQ',
+      },
+
+    ],
+  },
+];
+
+export default categories;
