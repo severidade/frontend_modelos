aba033c4fecd1f2413952d9797fa9f0b8b726515
severo <oliveira.severo@gmail.com>
2024-08-08 01:39:32 -0300

    refactor: :recycle: Modifica o nome das chaves no arquivo que simula o retorno da api

--- src/data/index2.ts
+++ src/data/index2.ts
@@ -1,58 +1,58 @@
 const categories = [
   {
     id: 1,
-    name: 'Adventure',
+    categoryName: 'Adventure',
     movies: [
       {
-        id: 1, title: 'Toy Story', released: 1995, embedId: 'nDe7uVm9TAk',
+        id: 1, movieTitle: 'Toy Story', released: 1995, embedId: 'nDe7uVm9TAk',
       },
       {
-        id: 2, title: 'Alice in Wonderland', released: 1951, embedId: 'yGCPhLXJ4hI',
+        id: 2, movieTitle: 'Alice in Wonderland', released: 1951, embedId: 'yGCPhLXJ4hI',
       },
       {
-        id: 3, title: 'Beetlejuice', released: 1988, embedId: 'kU1PAvdGeI8',
+        id: 3, movieTitle: 'Beetlejuice', released: 1988, embedId: 'kU1PAvdGeI8',
       },
       {
-        id: 4, title: 'The Incredibles', released: 2004, embedId: 'I7pVNcjclx8',
+        id: 4, movieTitle: 'The Incredibles', released: 2004, embedId: 'I7pVNcjclx8',
       },
       {
-        id: 5, title: 'The Powerpuff Girls', released: 1998, embedId: '7qmPEg8rgjA',
+        id: 5, movieTitle: 'The Powerpuff Girls', released: 1998, embedId: '7qmPEg8rgjA',
       },
     ],
   },
   {
     id: 2,
-    name: 'Comedy',
+    categoryName: 'Comedy',
     movies: [
       {
-        id: 6, title: 'The Flintstones', released: 1994, embedId: 'iStl4Al48BA',
+        id: 6, movieTitle: 'The Flintstones', released: 1994, embedId: 'iStl4Al48BA',
       },
       {
-        id: 7, title: 'Shrek', released: 2001, embedId: 'cbMgEtAIXPU',
+        id: 7, movieTitle: 'Shrek', released: 2001, embedId: 'cbMgEtAIXPU',
       },
       {
-        id: 8, title: 'Sponge Bob', released: 2004, embedId: 'py6aEUjT8uY',
+        id: 8, movieTitle: 'Sponge Bob', released: 2004, embedId: 'py6aEUjT8uY',
       },
     ],
   },
   {
     id: 3,
-    name: 'Sci-fi',
+    categoryName: 'Sci-fi',
     movies: [
       {
-        id: 9, title: 'The Fifth Element', released: 1997, embedId: 'VGxfLsCrW7I',
+        id: 9, movieTitle: 'The Fifth Element', released: 1997, embedId: 'VGxfLsCrW7I',
       },
       {
-        id: 10, title: 'Return of the Jedi', released: 1983, embedId: 'm7Xcpofvm7M',
+        id: 10, movieTitle: 'Return of the Jedi', released: 1983, embedId: 'm7Xcpofvm7M',
       },
       {
-        id: 11, title: 'He Man - The Masters of the Universe', released: 1987, embedId: 'D-FVPr7lNQM',
+        id: 11, movieTitle: 'He Man - The Masters of the Universe', released: 1987, embedId: 'D-FVPr7lNQM',
       },
       {
-        id: 12, title: 'Star Wars: The Phantom Menace', released: 1999, embedId: '0uqhAoMkFUc',
+        id: 12, movieTitle: 'Star Wars: The Phantom Menace', released: 1999, embedId: '0uqhAoMkFUc',
       },
       {
-        id: 13, title: 'The Matrix', released: 1999, embedId: 'i5jCFWlGlnQ',
+        id: 13, movieTitle: 'The Matrix', released: 1999, embedId: 'i5jCFWlGlnQ',
       },
     ],
   },
